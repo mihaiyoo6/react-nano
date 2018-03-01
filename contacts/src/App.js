cc5521f7c7cb4a1e4d71cee63d4039ffc9781c3e
Mihai Mijea <mmihai@suitsupply.com>
2018-03-01 07:38:46 +0100

    inish lesson 3

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
-
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts || [];
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>;
-  }
-}
-
+import ListContacts from './ListContacts';
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
+  }
   render() {
     return (
       <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]} />
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Richard' },
-          { name: 'Geoff' }
-        ]} />
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact} />
       </div>
     );
   }
---
+++ contacts/src/ListContacts.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import excapeRegEx from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+export default class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (value) => {
+    this.setState({ query: value });
+  }
+  clearQuery = () => {
+    this.updateQuery('');
+  }
+  render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+    let showingContacts = [];
+    if (query) {
+      const match = new RegExp(excapeRegEx(query), 'i');
+      showingContacts = contacts.filter(contact => match.test(contact.name));
+    } else {
+      showingContacts = contacts
+    }
+    showingContacts.sort(sortBy('name'));
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            type="text"
+            className="search-contacts"
+            value={query}
+            onChange={event => this.updateQuery(event.target.value)} />
+        </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+        <ol className='contact-list' >
+          {
+            showingContacts.map(contact => (
+              <li key={contact.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }} />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => onDeleteContact(contact)} >
+                  Remove
+						</button>
+              </li>
+            ))
+          }
+        </ol>
+      </div>
+    )
+  }
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+}
