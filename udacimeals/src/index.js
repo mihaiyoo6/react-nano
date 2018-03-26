ae7187bb0396523d5eb41d2b10619d50ac266985
Mihai Mijea <mijeamihai@yahoo.com>
2018-03-26 09:36:09 +0200

    redux at it's core

--- udacimeals/src/index.js
+++ udacimeals/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducer from './reducers';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
